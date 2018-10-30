c4a39b731eab312bd35b7e22d347cf6768434296
Dominic Prass <dominic.prass@googlemail.com>
2018-10-30 18:31:53 +0100

    Added some decoded panasonic ir codes

--- main.c
+++ main.c
@@ -19,8 +19,26 @@ volatile uint8_t elapsed = 0;
 #define PANASONIC_ONE_SPACE   (1244 - OFFSET)
 #define PANASONIC_ZERO_SPACE  ( 400 - OFFSET)
 
-//uint32_t one[] = {4, 0, 0, 4, 0, 1, 0, 0, 0, 8, 0, 9};
-const uint32_t one[] = {0b0100000000000100, 0b00000001000000000000100000001001};
+const uint32_t btn_menu[]	= {0x1004A4B};
+const uint32_t btn_apps[]	= {0x190F160};
+const uint32_t btn_exit[]	= {0x100CBCA};
+const uint32_t btn_back[]	= {0x1002B2A};
+	
+const uint32_t btn_up[]		= {0x190A938};
+const uint32_t btn_right[]	= {0x100F2F3};
+const uint32_t btn_down[]	= {0x100D2D3};
+const uint32_t btn_left[]	= {0x1007273};
+	
+const uint32_t btn_ok[]		= {0x1009293};
+const uint32_t btn_off[]	= {0x100BCBD};
+
+const uint32_t btn_p_up[]	= {0x1002C2D};
+const uint32_t btn_p_down[]	= {0x100ACAD};
+const uint32_t btn_v_up[]	= {0x1000405};
+const uint32_t btn_v_down[]	= {0x1008485};
+	
+const uint32_t btn_mute[]	= {0x4004, 0x1004C4D};
+
 
 ISR (TIMER1_COMPA_vect) {
 	elapsed = 1;
@@ -71,14 +89,14 @@ void send(const uint32_t * data)
 		enable_t0_38khz();
 		wait_for_x_t(PANASONIC_BIT_MARK);
 		disable_t0_38khz();
-		if (data[0] & mask)  wait_for_x_t(PANASONIC_ONE_SPACE) ;
+		if ((uint32_t)0x4004 & mask)  wait_for_x_t(PANASONIC_ONE_SPACE) ;
 		else                 wait_for_x_t(PANASONIC_ZERO_SPACE) ;
 	}
 	for (uint32_t  mask = 1UL << (32 - 1);  mask;  mask >>= 1) {
 		enable_t0_38khz();
 		wait_for_x_t(PANASONIC_BIT_MARK);
 		disable_t0_38khz();
-		if (data[1] & mask)  wait_for_x_t(PANASONIC_ONE_SPACE) ;
+		if (*data & mask)  wait_for_x_t(PANASONIC_ONE_SPACE) ;
 		else                 wait_for_x_t(PANASONIC_ZERO_SPACE) ;
 	}
 	enable_t0_38khz();
@@ -92,7 +110,7 @@ int main(void)
 	setup_t0_38khz();
     while (1) 
     {
-			send(one);
+			send(btn_off);
 			wait_for_x_t(50000);
     }
 }
