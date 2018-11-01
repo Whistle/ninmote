ec4a5f65f2ce22e11ef87b8948b1391e798fd1d6
Dominic Prass <dominic.prass@googlemail.com>
2018-11-01 18:32:12 +0100

    Added comments to carrier frequency setup routine

--- main.c
+++ main.c
@@ -83,11 +83,14 @@ inline void wait_for_x_t(uint16_t t) {
 }
 
 void setup_carrier_frequency() {
-	DDRD |= (1<<DDD5);
-	
 	TCCR0A = (1<<COM0A1) | (1<<COM0B1) | (1<<WGM01) | (1<<WGM00);
 	TCCR0B = (1<<WGM02);
+	
+	/* OC0B to output */
+	DDRD |= (1<<DDD5);
+	/* 1MHz / 26 = ~38461 Hz */
 	OCR0A = 26;
+	/* 20% duty cycle */
 	OCR0B = OCR0A / 5;
 }
 
